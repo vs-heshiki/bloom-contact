e597ddaa9544e6d558f696b8301a8533e436c070
Victor Heshiki <vs_heshiki@hotmail.com>
2025-08-29 12:41:05 -0300

    chore: add jest dependencies and configs

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'jest';
+
+const unit: Config = {
+  displayName: 'unit',
+  testMatch: ['**/tests/unit/*.test.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+const integration: Config = {
+  displayName: 'integration',
+  testMatch: ['**/tests/integration/*.test.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+const config: Config = {
+  projects: [unit, integration],
+};
+
+export default config;
