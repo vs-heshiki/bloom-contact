cfb6bbbb4fcc35b83eacc45e1f0c8f21f8fd31f6
Victor Heshiki <vs_heshiki@hotmail.com>
2025-08-29 10:28:20 -0300

    chore: install sequelize and config

---
+++ src/infra/database.ts
@@ -0,0 +1,28 @@
+import { Sequelize } from 'sequelize';
+
+const env = process.env.NODE_ENV || 'development';
+
+let sequelize: Sequelize;
+
+if (env === 'test') {
+  sequelize = new Sequelize('sqlite::memory:', { logging: false });
+} else {
+  const dialect = (process.env.DB_DIALECT || 'mysql') as any;
+  sequelize = new Sequelize(
+    process.env.DB_NAME || 'bloomcontact',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || 'secret',
+    {
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT || 3306),
+      dialect,
+      logging: false,
+      define: {
+        timestamps: true,
+        paranoid: true,
+      },
+    }
+  );
+}
+
+export default sequelize;
