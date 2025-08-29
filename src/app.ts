c3e68d152e0579bec39ffb3f7497ce67a3998f7f
Victor Heshiki <vs_heshiki@hotmail.com>
2025-08-29 12:39:29 -0300

    refactor: add configs to express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import contactsRoutes from './routes/contacts.routes';
+import { errorHandler } from './controllers/middlewares';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(contactsRoutes);
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
+
+app.use(errorHandler);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
-import app from './app';
-
 dotenv.config();
+import app from './app';
+import sequelize from './infra/database';
+import './infra/structs/contacts.table';
+import './infra/structs/phones.table';
 
 const port = Number(process.env.PORT || 7545);
 
 (async () => {
   try {
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(port, () => console.log(`API on http://localhost:${port}`));
   } catch (err) {
+    console.error('Database connection failed', err);
     process.exit(1);
   }
 })();
